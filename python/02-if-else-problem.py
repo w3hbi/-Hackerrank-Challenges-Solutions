664b67b27d2216208f795fd063b8c27e07ded39c
Mohamed Wahbi Yaakoub <75315350+w3hbi@users.noreply.github.com>
2022-08-15 14:00:01 +0100

    add python submittions (#1)

---
+++ python/02-if-else-problem.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+# main function ...
+if __name__ == '__main__':
+    n = int(input())
+    if n % 2 == 1:
+        print("Weird")
+    elif n % 2 == 0 and n in range(2, 6):
+        print("Not Weird")
+    elif n % 2 == 0 and n in range(6, 21):
+        print("Weird")
+    elif n % 2 == 0 and n > 20:
+        print("Not Weird")
---
+++ python/03-list-comprehensions.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+# Problem Statement:
+# You are given three integers x, y and z and representing 
+# the dimensions of a cuboid along with an integer n. 
+# Print a list of all possible coordinates given by '(i, j, k)'
+# on a 3D grid where the sum of i + j + k is not equal to n. 
+
+# Please use list comprehensions rather than multiple loops, as 
+# a learning exercise.
+
+if __name__ == '__main__':
+    # get inputs from user ...
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    # initialize a list to store the coordinates
+    L = []
+    
+    # List comprehension to generate all possible coordinates
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if i+j+k != n:
+                    L.append([i,j,k])
+                    
+    # print the list of coordinates
+    print(L)
